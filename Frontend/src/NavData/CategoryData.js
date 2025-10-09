03955e06d09ed9e40e79f4e0ba9017e67f8445ed
Rohan Maan <rohanmandal2208@gmail.com>
2025-10-09 12:49:21 +0530

    Added new Files

---
+++ Frontend/src/NavData/CategoryData.js
@@ -0,0 +1,18 @@
+export const categoryData = [
+  {
+    title: "For Employee",
+    image: "https://images.unsplash.com/photo-1758520144658-c87be518b87e?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    route: "/employees",
+  },
+  {
+    title: "For Clients",
+    image: "https://images.unsplash.com/photo-1758524055761-2d0c14766743?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    route: "/clients",
+  },
+  {
+    title: "Premium Gifts",
+    image: "https://images.unsplash.com/photo-1697719274531-5b647347a1ed?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    route: "/premium",
+  },
+  
+];
